a35dc238e84ab0075579f4c108dec1fafc85adf3
Andrew Capaldo <andycapaldo2@gmail.com>
2025-07-03 20:51:49 -0600

    style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div
-      className="font-[family-name:var(--font-geist-sans)] h-screen w-full overflow-hidden flex flex-col items-center justify-start py-4 bg-primary"
+      className="font-[family-name:var(--font-geist-sans)] min-h-[100dvh] w-full overflow-x-hidden flex flex-col items-center justify-start py-4 bg-primary"
       style={{
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='10' height='10'%3E%3Ccircle cx='1' cy='1' r='1' fill='%23333' /%3E%3C/svg%3E")`,
       }}
