85e2cffd2ee72236f55e34e5c04ba538b660f7c1
Andrew Capaldo <andycapaldo2@gmail.com>
2025-07-23 16:20:38 -0600

    Added Discord link

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,14 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMedium, faGithub, faTwitter, faXTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faMedium, faGithub, faTwitter, faXTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons'
 
 
 function Card() {
   return (
-    <div className='flex justify-center items-center w-full h-32 text-white w-full lg:max-w-3xl md:max-w-xl max-w-sm mx-auto mt-8 md:mt-4'>
+    <div className='flex justify-center items-center w-full h-32 text-white w-full lg:max-w-3xl md:max-w-xl max-w-sm mx-auto mt-12 md:mt-4'>
         <div className='flex flex-col md:flex-row items-center justify-center gap-8 w-full h-full'>
             <div className='w-1/2 md:w-auto'><DotExpandButton text="Github" icon={faGithub} link="https://github.com/qbittensor-labs" /></div>
             <div className='w-1/2 md:w-auto'><DotExpandButton text="X" icon={faXTwitter} link="https://x.com/qBitTensor" /></div>
             <div className='w-1/2 md:w-auto'><DotExpandButton text="Medium" icon={faMedium} link="https://medium.com/@qbittensorlabs/on-monday-july-7th-qbittensor-labs-will-be-launching-bittensors-first-quantum-subnet-aptly-named-d01964f3f8be" /></div>
+            <div className='w-1/2 md:w-auto'><DotExpandButton text="Discord" icon={faDiscord} link="https://discord.com/invite/ysA3VJMWNb" /></div>
         </div>
     </div>
   )
